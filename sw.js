a135120e01c944bbc007b9fa28491eef0e10f1e0
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-09 16:02:37 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "captapro-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./data.json",
+        "./manifest.webmanifest",
+        "./icon.svg"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
