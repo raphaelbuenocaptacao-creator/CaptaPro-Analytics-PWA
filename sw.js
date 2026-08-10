efa4d601dbd4ff9a24590dc322367f659f82627e
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-10 09:04:54 -0300

    Fix FluxoHub cache and always refresh analytics data

--- sw.js
+++ sw.js
@@ -1,23 +1,34 @@
-const CACHE = "captapro-v1";
+const CACHE = "fluxohub-v2";
+const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./data.json",
-        "./manifest.webmanifest",
-        "./icon.svg"
-      ])
-    )
+    caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
+  const url = new URL(event.request.url);
+  if (url.pathname.endsWith("/data.json")) {
+    event.respondWith(
+      fetch(event.request, { cache: "no-store" })
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
