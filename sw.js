87e279b57e7da86a8bfc98548a212c348e382c8e
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-23 10:27:33 -0300

    Atualiza cache PWA e força refresh da base atual

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fluxohub-v5";
+const CACHE = "fluxohub-v6";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
@@ -6,7 +6,7 @@ self.addEventListener("fetch", event => {
   const request=event.request;
   if(request.method!=="GET") return;
   const url=new URL(request.url);
-  if(url.pathname.endsWith("/data.json")){
+  if(url.pathname.endsWith("/data.json") || url.pathname.endsWith("/data-latest.json")){
     event.respondWith(fetch(request,{cache:"no-store"}).then(response=>{if(response.ok)caches.open(CACHE).then(c=>c.put(request,response.clone()));return response}).catch(()=>caches.match(request)));
     return;
   }
