b118b67cc294dcaaec027fbbc583f5631e5e54aa
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-23 10:43:59 -0300

    Atualiza cache do PWA para versao instalavel

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "fluxohub-v7";
-const APP_SHELL = ["./index.html", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "fluxohub-v8";
+const APP_SHELL = ["./index.html", "./manifest.webmanifest", "./icon.svg", "./app.js"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
