0de13dd9e09a012d72a806b9690944345310f1c6
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-23 12:38:04 -0300

    Limpa cache quebrado e publica base anual corrigida

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "captaup-v14-android2";
+const CACHE = "captaup-v15-datafix";
 const APP_SHELL = ["./index.html", "./manifest.webmanifest", "./icon.svg", "./captaup.css", "./captaup-data.js", "./captaup-main.js", "./captaup-pwa.js", "./data-2026-full.json", "./data-2026-full.part1.txt", "./data-2026-full.part2.txt", "./data-2026-full.part3.txt", "./data-2026-full.part4.txt", "./vendor/pako.part1.txt", "./vendor/pako.part2.txt", "./vendor/pako.part3.txt"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
